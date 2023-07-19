3b8b55f5ce502e35ecce47316c4d49090179525b
Zotman03 <zhenglin.ning@outlook.com>
2023-07-19 16:15:40 -0700

    Bouncy Ball added
    Atom moving around

---
+++ Assets/OrbitAroundPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OrbitAroundPlayer : MonoBehaviour
+{
+    public Transform player; // Reference to the player GameObject
+    public float orbitSpeed = 30f; // Adjust this value to control the orbit speed
+    // [SerializeField] private GameObject target;
+    [SerializeField] private float degreesPerSecond = 45;
+
+    void Update()
+    {
+        // Calculate the rotation axis based on the player's position and the orbiting object's position
+        Vector3 rotationAxis = player.position - transform.position;
+
+        // Rotate the object around the player
+        // transform.RotateAround(player.position, rotationAxis, orbitSpeed * Time.deltaTime);
+        transform.RotateAround(player.transform.position, Vector3.forward, degreesPerSecond * Time.deltaTime);
+    }
+}
