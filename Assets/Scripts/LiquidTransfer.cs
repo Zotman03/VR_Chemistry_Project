33b99b2e46000050127b0d0a1a8f5d68b38eeddf
Sharon <sharon.m0626@gmail.com>
2023-08-05 17:52:37 -0700

    Add glow to flask upon liquid mixture

--- Assets/Scripts/LiquidTransfer.cs
+++ Assets/Scripts/LiquidTransfer.cs
@@ -15,6 +15,10 @@ public class LiquidTransfer : MonoBehaviour
     [SerializeField]
     private float foamSmoothIncreaseRate = 0.2f;
 
+    // Material to be applied to the duplicated object
+    public Material newMaterial;
+    private bool canDuplicate = true;
+
     private void Start()
     {
         grabbableLiquid.isSocketed = true;
@@ -22,7 +26,7 @@ public class LiquidTransfer : MonoBehaviour
 
     void Update()
     {
-        if (socketInteractor.selectTarget)
+        if (socketInteractor.hasSelection)
         {
             float transferAmount = transferRate * Time.deltaTime;
 
@@ -37,6 +41,7 @@ public class LiquidTransfer : MonoBehaviour
                 {
                     socketLiquid.fillAmount = Mathf.Clamp(socketLiquid.fillAmount + grabbableLiquid.fillAmount, 0f, 1f);
                     grabbableLiquid.fillAmount = 0f;
+                    grabbableLiquid.isSocketed = false;
                 }
 
                 // Foam properties and color adjustments
@@ -52,11 +57,43 @@ public class LiquidTransfer : MonoBehaviour
                 Color grabTopColor = grabbableLiquid.liquidRenderer.material.GetColor("_TopColor");
                 socketLiquid.liquidRenderer.material.SetColor("_FoamColor", grabTopColor);
             }
+            else
+            {
+                grabbableLiquid.isSocketed = false;
+                if (canDuplicate == true && socketLiquid.fillAmount >= 0.99f)
+                {
+                    DuplicateObject();
+                    canDuplicate = false;
+                }
+            }
         }
-        else if (grabbableLiquid.isSocketed == true)
+    }
+
+    void DuplicateObject()
+    {
+        // Create a duplicate of the current object
+        GameObject duplicate = Instantiate(gameObject, transform.position, transform.rotation);
+        // Set the parent of the duplicate to the original object's parent
+        duplicate.transform.parent = transform.parent;
+        // Make the duplicate slightly larger
+        duplicate.transform.localScale = transform.localScale * 1.05f;
+        // Remove the script from the duplicated object
+        Destroy(duplicate.GetComponent<LiquidTransfer>());
+        // Remove the XRSocketInteractor from the duplicate
+        XRSocketInteractor socketInteractor = duplicate.GetComponent<XRSocketInteractor>();
+        if (socketInteractor != null)
         {
-            grabbableLiquid.isSocketed = false;
+            Destroy(socketInteractor);
         }
+        // Delete all children from the duplicate
+        foreach (Transform child in duplicate.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        // Apply the new material to the duplicate object
+        Renderer rend = duplicate.GetComponent<Renderer>();
+        if (rend != null)
+            rend.material = newMaterial;
     }
 }
 
