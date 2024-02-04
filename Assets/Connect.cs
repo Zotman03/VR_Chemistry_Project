85620e4e5e08951572868d3660bf26aaf5fdc6c3
Zotman03 <zhenglin.ning@outlook.com>
2024-02-03 19:55:02 -0800

    Update restructured Disconnect bond
    - Removed useless code
    - Optimized disconnection interaction
    - Disconnect without problem

--- Assets/Connect.cs
+++ Assets/Connect.cs
@@ -1,138 +1,3 @@
-// using System.Collections;
-// using UnityEngine;
-
-// public class Connect : MonoBehaviour
-// {
-//     public bool isAlreadyConnected = false;
-//     private GameObject connectedObject;
-//     public float maxDistance = 5.0f;
-
-//     void OnCollisionEnter(Collision collision)
-//     {
-//         Debug.Log("OnCollisionEnter: Collided with " + collision.gameObject.name);
-
-//         if (isAlreadyConnected)
-//         {
-//             Debug.Log("Already connected, ignoring collision");
-//             return;
-//         }
-
-//         Connect otherSphere = collision.gameObject.GetComponent<Connect>();
-//         //NAConnect naSphere = collision.gameObject.GetComponent<NAConnect>();
-
-//         if (otherSphere && !otherSphere.isAlreadyConnected)
-//         {
-//             Debug.Log("Bonding with other Connect object: " + collision.gameObject.name);
-//             Bond(collision.gameObject);
-//             otherSphere.Bonded(this.gameObject);
-//             isAlreadyConnected = true;
-//         }
-//         // else if (naSphere && !naSphere.isAlreadyConnected)
-//         // {
-//         //     Debug.Log("Bonding with NAConnect object: " + collision.gameObject.name);
-//         //     Bond(collision.gameObject);
-//         //     naSphere.isAlreadyConnected = true;
-//         //     isAlreadyConnected = true;
-//         // }
-//     }
-
-//     void Bond(GameObject other)
-//     {
-//         Debug.Log("Creating bond with: " + other.name);
-//         FixedJoint joint = gameObject.AddComponent<FixedJoint>();
-//         joint.connectedBody = other.GetComponent<Rigidbody>();
-
-//         GameObject parentCL = gameObject.transform.parent.gameObject;
-//         Debug.Log(parentCL);
-//         GameObject otherParentCL = other.transform.parent.gameObject;
-//         Debug.Log(otherParentCL);
-
-//         // Check if the parent CL GameObjects have Rigidbodies and are not already connected
-//         Rigidbody parentRb = parentCL.GetComponent<Rigidbody>();
-//         Rigidbody otherParentRb = otherParentCL.GetComponent<Rigidbody>();
-
-//         if (parentRb != null && otherParentRb != null)
-//         {
-//             FixedJoint parentJoint = parentCL.AddComponent<FixedJoint>();
-//             parentJoint.connectedBody = otherParentRb;
-
-//             gameObject.GetComponent<Connect>().isAlreadyConnected = true;
-//             other.GetComponent<Connect>().isAlreadyConnected = true;
-//         }
-//         connectedObject = other;
-//     }
-
-//     public void Bonded(GameObject other)
-//     {
-//         Debug.Log("Bonded called by: " + other.name);
-//         isAlreadyConnected = true;
-//         connectedObject = other;
-//     }
-
-//     public void BreakBond()
-//     {
-//         Debug.Log("Breaking bond");
-//         FixedJoint joint = GetComponent<FixedJoint>();
-//         if (joint)
-//         {
-//             Destroy(joint);
-//         }
-//         if (connectedObject != null)
-//         {
-//             Connect otherConnect = connectedObject.GetComponent<Connect>();
-//             if (otherConnect != null)
-//             {
-//                 otherConnect.BreakBondFromOther();
-//             }
-//             connectedObject = null;
-//         }
-//         StartCoroutine(ResetAfterPhysicsUpdate());
-//     }
-
-//     public void BreakBondFromOther()
-//     {
-//         Debug.Log("BreakBondFromOther called");
-//         FixedJoint joint = GetComponent<FixedJoint>();
-//         if (joint)
-//         {
-//             Destroy(joint);
-//         }
-//         connectedObject = null;
-//         isAlreadyConnected = false;
-//     }
-
-//     private IEnumerator ResetAfterPhysicsUpdate()
-//     {
-//         yield return new WaitForFixedUpdate();
-//         Debug.Log("Physics update reset");
-//         isAlreadyConnected = false;
-//         Rigidbody rb = GetComponent<Rigidbody>();
-//         if (rb != null)
-//         {
-//             rb.isKinematic = false;
-//             rb.velocity = Vector3.zero;
-//             rb.angularVelocity = Vector3.zero;
-//         }
-//     }
-//     void Update()
-//     {
-//         if (isAlreadyConnected && connectedObject != null)
-//         {
-//             float distance = Vector3.Distance(transform.position, connectedObject.transform.position);
-
-//             if (distance > maxDistance)
-//             {
-//                 BreakBond();
-//                 Connect otherConnect = connectedObject.GetComponent<Connect>();
-//                 if (otherConnect != null)
-//                 {
-//                     otherConnect.BreakBondFromOther();
-//                 }
-//             }
-//         }
-//     }
-// }
-
 using System.Collections;
 using UnityEngine;
 
@@ -165,6 +30,15 @@ public class Connect : MonoBehaviour
     }
     void Update()
     {
+        if (isAlreadyConnected && connectedObject != null)
+        {
+            float distance = Vector3.Distance(subGameObjectA.transform.position, connectedObject.transform.position);
+            if (distance > 0.5f) // 3 feet in meters
+            {
+                BreakBond();
+                return; // Exit the method to prevent trying to reconnect in the same frame
+            }
+        }
         // Only proceed if not already connected and the subGameObjectA is assigned
         if (!isAlreadyConnected && subGameObjectA != null)
         {
@@ -197,20 +71,6 @@ public class Connect : MonoBehaviour
             }
         }
     }
-
-
-    // void Bond(GameObject other, GameObject initiatingChild)
-    // {
-    //     Debug.Log("Creating bond with: " + other.name);
-
-    //     // Use the initiatingChild to add the FixedJoint to the correct sub-game object
-    //     FixedJoint joint = initiatingChild.AddComponent<FixedJoint>();
-    //     joint.connectedBody = other.GetComponent<Rigidbody>();
-
-    //     // Now we consider the whole GameObject connected
-    //     isAlreadyConnected = true;
-    //     connectedObject = other;
-    // }
 
     public void Bonded(GameObject other, GameObject initiatingChild)
     {
