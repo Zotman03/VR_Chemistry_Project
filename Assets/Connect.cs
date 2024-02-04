11f2423f994b2bcd3c4fff16badfada32ba36889
Zotman03 <zhenglin.ning@outlook.com>
2024-02-03 17:19:14 -0800

    Restructure Connect and Disconnect
    - RigidBody on the Electrons
    - Attached connect.cs script to the overall molecule instead of having it on the electrons
    - Able to connect, grab onto the molecule. The molecule can be held in either way.
    - Molecule won't fly into the horizon when thrown

--- Assets/Connect.cs
+++ Assets/Connect.cs
@@ -1,110 +1,153 @@
-// // using System.Collections;
-// // using System.Collections.Generic;
-// // using UnityEngine;
+// using System.Collections;
+// using UnityEngine;
 
-// // public class Connect : MonoBehaviour
-// // {
-// //     public bool isAlreadyConnected = false;  // Flag to track whether this object is already connected
-// //     int connect = 0;
-// //     private GameObject connectedObject;
+// public class Connect : MonoBehaviour
+// {
+//     public bool isAlreadyConnected = false;
+//     private GameObject connectedObject;
+//     public float maxDistance = 5.0f;
 
-// //     void OnCollisionEnter(Collision collision)
-// //     {
-// //         if (isAlreadyConnected)
-// //         {
-// //             return;
-// //         }
+//     void OnCollisionEnter(Collision collision)
+//     {
+//         Debug.Log("OnCollisionEnter: Collided with " + collision.gameObject.name);
 
-// //         Connect otherSphere = collision.gameObject.GetComponent<Connect>();
-// //         NAConnect naSphere = collision.gameObject.GetComponent<NAConnect>();
+//         if (isAlreadyConnected)
+//         {
+//             Debug.Log("Already connected, ignoring collision");
+//             return;
+//         }
 
-// //         if (otherSphere && !otherSphere.isAlreadyConnected)
-// //         {
-// //             Bond(collision.gameObject);
-// //             otherSphere.Bonded();
-// //             isAlreadyConnected = true;
-// //         }
-// //         else if (naSphere && !naSphere.isAlreadyConnected)
-// //         {
-// //             Bond(collision.gameObject);
-// //             naSphere.isAlreadyConnected = true;
-// //             isAlreadyConnected = true;
-// //         }
-// //     }
+//         Connect otherSphere = collision.gameObject.GetComponent<Connect>();
+//         //NAConnect naSphere = collision.gameObject.GetComponent<NAConnect>();
 
-// //     void Bond(GameObject other)
-// //     {
-// //         FixedJoint joint = gameObject.AddComponent<FixedJoint>();
-// //         joint.connectedBody = other.GetComponent<Rigidbody>();
-// //         connect++;
-// //     }
+//         if (otherSphere && !otherSphere.isAlreadyConnected)
+//         {
+//             Debug.Log("Bonding with other Connect object: " + collision.gameObject.name);
+//             Bond(collision.gameObject);
+//             otherSphere.Bonded(this.gameObject);
+//             isAlreadyConnected = true;
+//         }
+//         // else if (naSphere && !naSphere.isAlreadyConnected)
+//         // {
+//         //     Debug.Log("Bonding with NAConnect object: " + collision.gameObject.name);
+//         //     Bond(collision.gameObject);
+//         //     naSphere.isAlreadyConnected = true;
+//         //     isAlreadyConnected = true;
+//         // }
+//     }
 
-// //     public void Bonded()
-// //     {
-// //         isAlreadyConnected = true;
-// //     }
+//     void Bond(GameObject other)
+//     {
+//         Debug.Log("Creating bond with: " + other.name);
+//         FixedJoint joint = gameObject.AddComponent<FixedJoint>();
+//         joint.connectedBody = other.GetComponent<Rigidbody>();
 
-// //     void Bond(GameObject other)
-// //     {
-// //         FixedJoint joint = gameObject.AddComponent<FixedJoint>();
-// //         joint.connectedBody = other.GetComponent<Rigidbody>();
-// //         connect++;
+//         GameObject parentCL = gameObject.transform.parent.gameObject;
+//         Debug.Log(parentCL);
+//         GameObject otherParentCL = other.transform.parent.gameObject;
+//         Debug.Log(otherParentCL);
 
-// //         // Set the connected object
-// //         connectedObject = other;
-// //     }
+//         // Check if the parent CL GameObjects have Rigidbodies and are not already connected
+//         Rigidbody parentRb = parentCL.GetComponent<Rigidbody>();
+//         Rigidbody otherParentRb = otherParentCL.GetComponent<Rigidbody>();
 
-// //     public void BreakBond()
-// //     {
-// //         FixedJoint joint = GetComponent<FixedJoint>();
-// //         if (joint)
-// //         {
-// //             Destroy(joint);
-// //             //joint.connectedBody = null;
-// //         }
-// //         StartCoroutine(ResetAfterPhysicsUpdate());
-// //     }
-// //     public void Disconnect()
-// //     {
-// //         BreakBond();
-// //         // Inform the other object to break the bond as well
-// //         if (connectedObject != null)
-// //         {
-// //             connectedObject.GetComponent<Connect>().BreakBond();
-// //         }
-// //     }
-// //     private IEnumerator ResetAfterPhysicsUpdate()
-// //     {
-// //         yield return new WaitForFixedUpdate();
-// //         isAlreadyConnected = false;
-// //         Rigidbody rb = GetComponent<Rigidbody>();
-// //         if (rb != null)
-// //         {
-// //             rb.isKinematic = false;
-// //             rb.velocity = Vector3.zero;
-// //             rb.angularVelocity = Vector3.zero;
-// //         }
+//         if (parentRb != null && otherParentRb != null)
+//         {
+//             FixedJoint parentJoint = parentCL.AddComponent<FixedJoint>();
+//             parentJoint.connectedBody = otherParentRb;
 
-// //         // Optionally, if you need to force the objects to separate, you can apply a small force or change their positions
-// //         //rb.AddForce(Vector3.up * 0.1f, ForceMode.VelocityChange); // Example force
+//             gameObject.GetComponent<Connect>().isAlreadyConnected = true;
+//             other.GetComponent<Connect>().isAlreadyConnected = true;
+//         }
+//         connectedObject = other;
+//     }
 
-// //         // If there's another object that was bonded, reset its state as well
-// //         // You'll need a reference to the connected object to do this
-// //     }
-// // }
+//     public void Bonded(GameObject other)
+//     {
+//         Debug.Log("Bonded called by: " + other.name);
+//         isAlreadyConnected = true;
+//         connectedObject = other;
+//     }
+
+//     public void BreakBond()
+//     {
+//         Debug.Log("Breaking bond");
+//         FixedJoint joint = GetComponent<FixedJoint>();
+//         if (joint)
+//         {
+//             Destroy(joint);
+//         }
+//         if (connectedObject != null)
+//         {
+//             Connect otherConnect = connectedObject.GetComponent<Connect>();
+//             if (otherConnect != null)
+//             {
+//                 otherConnect.BreakBondFromOther();
+//             }
+//             connectedObject = null;
+//         }
+//         StartCoroutine(ResetAfterPhysicsUpdate());
+//     }
+
+//     public void BreakBondFromOther()
+//     {
+//         Debug.Log("BreakBondFromOther called");
+//         FixedJoint joint = GetComponent<FixedJoint>();
+//         if (joint)
+//         {
+//             Destroy(joint);
+//         }
+//         connectedObject = null;
+//         isAlreadyConnected = false;
+//     }
+
+//     private IEnumerator ResetAfterPhysicsUpdate()
+//     {
+//         yield return new WaitForFixedUpdate();
+//         Debug.Log("Physics update reset");
+//         isAlreadyConnected = false;
+//         Rigidbody rb = GetComponent<Rigidbody>();
+//         if (rb != null)
+//         {
+//             rb.isKinematic = false;
+//             rb.velocity = Vector3.zero;
+//             rb.angularVelocity = Vector3.zero;
+//         }
+//     }
+//     void Update()
+//     {
+//         if (isAlreadyConnected && connectedObject != null)
+//         {
+//             float distance = Vector3.Distance(transform.position, connectedObject.transform.position);
+
+//             if (distance > maxDistance)
+//             {
+//                 BreakBond();
+//                 Connect otherConnect = connectedObject.GetComponent<Connect>();
+//                 if (otherConnect != null)
+//                 {
+//                     otherConnect.BreakBondFromOther();
+//                 }
+//             }
+//         }
+//     }
+// }
 
 using System.Collections;
 using UnityEngine;
 
 public class Connect : MonoBehaviour
 {
+    public GameObject subGameObjectA; // Assign in the inspector, e.g., A or C
+    //public GameObject subGameObjectB; // Assign in the inspector, e.g., B or D
     public bool isAlreadyConnected = false;
-    private GameObject connectedObject;
+    private GameObject connectedObject; // This will reference the other parent GameObject we're connected to
     public float maxDistance = 5.0f;
 
-    void OnCollisionEnter(Collision collision)
+    // This method is now public and meant to be called from the child GameObjects
+    public void OnCollisionEnterChild(GameObject child, Collision collision)
     {
-        Debug.Log("OnCollisionEnter: Collided with " + collision.gameObject.name);
+        Debug.Log($"{child.name} collided with {collision.gameObject.name}");
 
         if (isAlreadyConnected)
         {
@@ -112,66 +155,101 @@ public class Connect : MonoBehaviour
             return;
         }
 
-        Connect otherSphere = collision.gameObject.GetComponent<Connect>();
-        //NAConnect naSphere = collision.gameObject.GetComponent<NAConnect>();
-
-        if (otherSphere && !otherSphere.isAlreadyConnected)
+        Connect other = collision.transform.parent.GetComponent<Connect>();
+        if (other != null && !other.isAlreadyConnected)
         {
-            Debug.Log("Bonding with other Connect object: " + collision.gameObject.name);
-            Bond(collision.gameObject);
-            otherSphere.Bonded(this.gameObject);
-            isAlreadyConnected = true;
+            Debug.Log("Bonding initiated by child");
+            Bond(other.gameObject, child);
+            other.Bonded(this.gameObject, child); // Assuming other.Bonded() is adjusted accordingly
         }
-        // else if (naSphere && !naSphere.isAlreadyConnected)
-        // {
-        //     Debug.Log("Bonding with NAConnect object: " + collision.gameObject.name);
-        //     Bond(collision.gameObject);
-        //     naSphere.isAlreadyConnected = true;
-        //     isAlreadyConnected = true;
-        // }
     }
-
-    void Bond(GameObject other)
+    void Update()
     {
-        Debug.Log("Creating bond with: " + other.name);
-        FixedJoint joint = gameObject.AddComponent<FixedJoint>();
-        joint.connectedBody = other.GetComponent<Rigidbody>();
-
-        GameObject parentCL = gameObject.transform.parent.gameObject;
-        Debug.Log(parentCL);
-        GameObject otherParentCL = other.transform.parent.gameObject;
-        Debug.Log(otherParentCL);
-
-        // Check if the parent CL GameObjects have Rigidbodies and are not already connected
-        Rigidbody parentRb = parentCL.GetComponent<Rigidbody>();
-        Rigidbody otherParentRb = otherParentCL.GetComponent<Rigidbody>();
-
-        if (parentRb != null && otherParentRb != null)
+        // Only proceed if not already connected and the subGameObjectA is assigned
+        if (!isAlreadyConnected && subGameObjectA != null)
         {
-            FixedJoint parentJoint = parentCL.AddComponent<FixedJoint>();
-            parentJoint.connectedBody = otherParentRb;
+            GameObject closestTarget = null;
+            float closestDistance = 0.01f; // Use maxDistance as the initial comparison base
 
-            gameObject.GetComponent<Connect>().isAlreadyConnected = true;
-            other.GetComponent<Connect>().isAlreadyConnected = true;
+            // Find all colliders within maxDistance from subGameObjectA
+            Collider[] hitColliders = Physics.OverlapSphere(subGameObjectA.transform.position, 0.01f);
+            foreach (var hitCollider in hitColliders)
+            {
+                Connect otherConnect = hitCollider.GetComponentInParent<Connect>();
+                // Check if the hitCollider's parent GameObject is a valid connection target
+                if (otherConnect != null && otherConnect != this && !otherConnect.isAlreadyConnected)
+                {
+                    float distance = Vector3.Distance(subGameObjectA.transform.position, hitCollider.transform.position);
+                    if (distance < closestDistance)
+                    {
+                        closestDistance = distance;
+                        closestTarget = hitCollider.gameObject;
+                    }
+                }
+            }
+
+            // If a closest target is found, attempt to bond
+            if (closestTarget != null)
+            {
+                Debug.Log("Attempting to bond with target based on proximity");
+                Bond(closestTarget.transform.parent.gameObject, subGameObjectA);
+                closestTarget.transform.parent.GetComponent<Connect>().Bonded(gameObject, subGameObjectA);
+            }
         }
-        connectedObject = other;
     }
 
-    public void Bonded(GameObject other)
+
+    // void Bond(GameObject other, GameObject initiatingChild)
+    // {
+    //     Debug.Log("Creating bond with: " + other.name);
+
+    //     // Use the initiatingChild to add the FixedJoint to the correct sub-game object
+    //     FixedJoint joint = initiatingChild.AddComponent<FixedJoint>();
+    //     joint.connectedBody = other.GetComponent<Rigidbody>();
+
+    //     // Now we consider the whole GameObject connected
+    //     isAlreadyConnected = true;
+    //     connectedObject = other;
+    // }
+
+    public void Bonded(GameObject other, GameObject initiatingChild)
     {
         Debug.Log("Bonded called by: " + other.name);
         isAlreadyConnected = true;
         connectedObject = other;
     }
 
+    void Bond(GameObject other, GameObject initiatingChild)
+    {
+        Debug.Log($"Creating bond with: {other.name}, initiated by {initiatingChild.name}");
+
+        // Ensure this object has a Rigidbody
+        Rigidbody thisRb = initiatingChild.transform.parent.GetComponent<Rigidbody>() ?? initiatingChild.transform.parent.gameObject.AddComponent<Rigidbody>();
+
+        // Add or get FixedJoint on initiating child
+        FixedJoint joint = initiatingChild.AddComponent<FixedJoint>();
+        joint.connectedBody = other.GetComponent<Rigidbody>();
+
+        // Optionally, ensure the other object is prepared to maintain connection
+        // This step depends on your specific requirements and setup
+        Rigidbody otherRb = other.GetComponent<Rigidbody>() ?? other.AddComponent<Rigidbody>();
+        FixedJoint otherJoint = other.AddComponent<FixedJoint>();
+        otherJoint.connectedBody = thisRb;
+
+        isAlreadyConnected = true;
+        connectedObject = other;
+    }
+
+
     public void BreakBond()
     {
         Debug.Log("Breaking bond");
-        FixedJoint joint = GetComponent<FixedJoint>();
-        if (joint)
+        // Breaking the bond now should consider all FixedJoints in children
+        foreach (FixedJoint joint in GetComponentsInChildren<FixedJoint>())
         {
             Destroy(joint);
         }
+
         if (connectedObject != null)
         {
             Connect otherConnect = connectedObject.GetComponent<Connect>();
@@ -187,8 +265,7 @@ public class Connect : MonoBehaviour
     public void BreakBondFromOther()
     {
         Debug.Log("BreakBondFromOther called");
-        FixedJoint joint = GetComponent<FixedJoint>();
-        if (joint)
+        foreach (FixedJoint joint in GetComponentsInChildren<FixedJoint>())
         {
             Destroy(joint);
         }
@@ -201,30 +278,6 @@ public class Connect : MonoBehaviour
         yield return new WaitForFixedUpdate();
         Debug.Log("Physics update reset");
         isAlreadyConnected = false;
-        Rigidbody rb = GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            rb.isKinematic = false;
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-        }
-    }
-    void Update()
-    {
-        if (isAlreadyConnected && connectedObject != null)
-        {
-            float distance = Vector3.Distance(transform.position, connectedObject.transform.position);
-
-            if (distance > maxDistance)
-            {
-                BreakBond();
-                Connect otherConnect = connectedObject.GetComponent<Connect>();
-                if (otherConnect != null)
-                {
-                    otherConnect.BreakBondFromOther();
-                }
-            }
-        }
     }
 }
 
