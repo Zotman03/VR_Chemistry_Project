e9776a0c066daf4031fb54970e1ffd1e041f54c5
Zotman03 <zhenglin.ning@outlook.com>
2024-02-03 20:46:54 -0800

    Update Global ChemistryData
    - Removed testing code
    - Update the number and type of molecules before and after connect and disconnect

--- Assets/ChildCollider.cs
+++ Assets/ChildCollider.cs
@@ -4,8 +4,6 @@ public class ChildCollider : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        // Call the parent's method to handle the collision
-        // Make sure your parent GameObject has a script component that contains this method
         GetComponentInParent<Connect>().OnCollisionEnterChild(gameObject, collision);
     }
 }
--- Assets/MolSpawner.cs
+++ Assets/MolSpawner.cs
@@ -1,22 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class MolSpawner : MonoBehaviour
-// {
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-
-//     }
-
-//     // Update is called once per frame
-//     void Update()
-//     {
-
-//     }
-// }
-
 using UnityEngine;
 
 public class MolSpawner : MonoBehaviour
