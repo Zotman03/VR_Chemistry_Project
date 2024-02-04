7d6e35c1de7269ff4ebb858a655113946803f8ad
Zotman03 <zhenglin.ning@outlook.com>
2024-02-03 20:26:01 -0800

    Removed Platform
    - Removed Platform
    - Plant different molecules at different positions
    - Changed color

--- Assets/MolSpawner.cs
+++ Assets/MolSpawner.cs
@@ -22,6 +22,7 @@ using UnityEngine;
 public class MolSpawner : MonoBehaviour
 {
     public GameObject spherePrefab;
+    public GameObject otherspherePrefab;
 
     void Start()
     {
@@ -34,9 +35,14 @@ public class MolSpawner : MonoBehaviour
         if (chemistryData != null)
         {
             int numberOfSpheres = Mathf.FloorToInt(chemistryData.molesOfSubstanceOne);
+            int numberOfSpheres1 = Mathf.FloorToInt(chemistryData.molesOfSubstanceTwo);
             for (int i = 0; i < numberOfSpheres; i++)
             {
-                Instantiate(spherePrefab, new Vector3(i * 2.0f, 0, 0), Quaternion.identity);
+                Instantiate(spherePrefab, new Vector3(i * 2.0f, 1, 0), Quaternion.identity);
+            }
+            for (int i = 0; i < numberOfSpheres1; i++)
+            {
+                Instantiate(otherspherePrefab, new Vector3(i * 4.0f, 1, 5), Quaternion.identity);
             }
         }
     }
